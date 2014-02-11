85c7d938cdf2f6f5d2cdd7bf4a5caeb9398559ac
miolini <miolini@gmail.com>
2014-02-12 00:49:30 +0400

    revert sync for buffered channel

--- chan_bench_test.go
+++ chan_bench_test.go
@@ -1,15 +1,23 @@
 package main
 
 import "testing"
+import "sync"
 
 func BenchmarkChannels(b *testing.B) {
     ch := make(chan bool, 10000)
+
+    var done sync.WaitGroup
+    done.Add(1)
+
     go func() {
         for _ = range ch {
         }
+        done.Done()
     }()
+
     for i := 0; i < b.N; i++ {
         ch <- true
     }
     close(ch)
+    done.Wait()
 }
