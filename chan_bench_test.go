7f0cf415a81bc4c59ec3523b7f8a9a7287181189
miolini <miolini@gmail.com>
2014-02-12 00:45:15 +0400

    optimizing bench loops

--- chan_bench_test.go
+++ chan_bench_test.go
@@ -1,27 +1,15 @@
 package main
 
 import "testing"
-import "sync"
 
 func BenchmarkChannels(b *testing.B) {
     ch := make(chan bool, 10000)
-
-    waitGroup := sync.WaitGroup{}
-    waitGroup.Add(2)
-
     go func() {
-        for i := 0; i < b.N; i++ {
-            ch <- true
+        for _ = range ch {
         }
-        waitGroup.Done()
     }()
-
-    go func() {
-        for i := 0; i < b.N; i++ {
-            <- ch
-        }
-        waitGroup.Done()
-    }()
-
-    waitGroup.Wait()
+    for i := 0; i < b.N; i++ {
+        ch <- true
+    }
+    close(ch)
 }
